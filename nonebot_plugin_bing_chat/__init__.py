133ab37907fd072f81e3757754fb6ff4d0681142
Harry <Harry0259@163.com>
2023-03-25 23:02:08 +0800

    Bug fix

--- nonebot_plugin_bing_chat/__init__.py
+++ nonebot_plugin_bing_chat/__init__.py
@@ -1,4 +1,4 @@
 from .common.utils import logger
 from .onebotv11.main import logger
 
-logger.opt(colors=True).warning('<red>你使用的是开发版，可能会出现未知错误，请及时反馈</red>')
+# logger.opt(colors=True).warning('<red>你使用的是开发版，可能会出现未知错误，请及时反馈</red>')
